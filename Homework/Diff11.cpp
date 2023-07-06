15f9f20fe8f377538c7ecef29d52b2f00379c67d
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-07-06 19:20:37 +0500

    Add files via upload

---
+++ Homework/Diff11.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int Diff11() {
+    int h1,m1,s1,h2,m2,s2;
+    cin >> h1 >> m1 >> s1 >> h2 >> m2 >> s2;
+    cout << (h2*60*60 - h1*60*60) + (m2*60-m1*60) + (s2-s1);
+    return 0;
+}
+
+
