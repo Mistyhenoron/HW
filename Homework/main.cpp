b2379416f1e45300c26a9234e4d77be5df2fa67e
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-07-09 00:25:58 +0500

    Add files via upload

---
+++ Homework/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int k;
+    cin >> k;
+    if (k%4 == 0 || k == 1) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+      return 0;
+}
+
