c0d6803d11152a5b97969afd4034a534560f728f
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-07-12 20:25:23 +0500

    Add files via upload

---
+++ Homework/main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int x, h, m, s;
+    cin >> x;
+    h = x / 3600 % 24;
+    m =  x / 60 % 60;
+    s = x % 60;
+    if (m < 10) {
+        if (s < 10) {
+            cout << h << ":" << '0' << m << ":" << '0' << s;
+        }
+        else {
+            cout <<  h << ":" << '0' << m << ":" << s;
+        }
+    }
+    else if (m >= 10) {
+        if (s < 10) {
+            cout << h << ":" << m << ":" << '0' << s;
+        }
+        else {
+            cout << h << ":" << m << ":" << s;
+        }
+    }
+    return 0;
+}
+
+
