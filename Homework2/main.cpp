85abcb448c011bea8bb1235a02c1e63e7d729657
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-07-18 08:47:51 +0500

    Add files via upload

---
+++ Homework2/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n,k,i=0;
+    cin >> n >> k;
+    while (i<k){
+        if (n%200 == 0) {
+            n = n/200;
+            i++;}
+        else {
+            n = n * 1000 + 200;
+            i++;
+    }
+    cout << n;
+    }
+    return 0;
+}
+
+
