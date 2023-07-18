82ac5825d653e44a3b1befaaa4ebc0702aded51a
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-07-18 21:28:24 +0500

    Add files via upload

---
+++ Homework2/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n,i=0,k,j=0;
+    cin >> n;
+    while (n>j) {
+        cin >> k;
+        if (k>10) {
+            i = i+k-10;
+        }
+        j++;}
+    cout << i;
+    return 0;
+}
