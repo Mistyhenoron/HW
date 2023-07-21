d41a3e5a8251e1f633e515f53ff3156fd553adf9
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-07-21 11:40:01 +0500

    Add files via upload

---
+++ Homework2/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+int n,k,f;
+cin >> n >> k;
+int s = 0;
+for (int i = 1;i<=n;i++) {
+    for (int j = 1; j <=k; j++) {
+        f = i*100 + j;
+        s= s + f;
+    }
+}
+cout << s;
+    return 0;
+}
+
+
+
+
