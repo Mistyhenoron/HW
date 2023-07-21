35f8025c289ad365dbc8f936c9a1f42d5e29cc35
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-07-21 17:42:41 +0500

    Add files via upload

---
+++ Homework2/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,c=0;
+    cin >> a >> b;
+    for (;a<=b;a++){
+        c++;
+    }
+    cout << c;
+
+    return 0;
+}
+
+
+
+
