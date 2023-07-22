f913f911a2c43d887a2cab374190dbdb03237b58
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-07-22 12:04:12 +0500

    Create Savings.cpp

---
+++ Homework2/Savings.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,d=0,s=0;
+    cin >> n;
+    while(s<n){
+        d++;
+        s+=d;
+    }
+    cout << d;
+    return 0;
+}
+
+
+
