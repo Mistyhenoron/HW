d192a992edc3ff18690431db40b52d0ab719654c
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-07-24 10:56:29 +0500

    Create Fullhouse.cpp

---
+++ Homework3/Fullhouse.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a[5];
+    for (int i = 0;i<5;i++){
+        cin >> a[i];
+    }
+    sort(a,a+5);
+    if((a[0]==a[1]&& a[1]==a[2]&& a[3]==a[4])||a[0]==a[1]&& a[2]==a[3]&& a[3]==a[4]) {
+        cout << "Yes";
+    }
+    else {
+        cout << "No";
+    }
+    return 0;
+}
