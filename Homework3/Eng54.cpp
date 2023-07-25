f7dae892cda588e367186e7f12a3dfac8843535c
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-07-25 18:30:32 +0500

    Update Eng54.cpp

--- Homework3/Eng54.cpp
+++ Homework3/Eng54.cpp
@@ -1,1 +1,20 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+int n,x=0;
+cin >> n;
+string a[n];
+for(int i = 0;i<n;i++){
+    cin >> a[i];
+    if (a[i]=="and"|| a[i]=="not" || a[i]=="that" || a[i]=="the" || a[i]=="you") {
+        x++;
+    }
+}
+if (x>=1){
+    cout << "Yes";
+}
+else {
+    cout << "No";
+}
+return 0;
+}
