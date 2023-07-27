96b9f37fffde24a360ea1820db9769afb341c412
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-07-27 23:11:01 +0500

    Update POW38.cpp

--- Homework2/POW38.cpp
+++ Homework2/POW38.cpp
@@ -1,17 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
-    int a,b,c;
-    cin >> a >> b >> c;
-    if (pow(a,c)<pow(b,c)) {
-        cout << " < ";
-    }
-    else if (pow(a,c) > pow(b,c)) {
+int main(){
+int a,b,c;
+cin >> a >> b >> c;
+if (c%2==0){
+    if (abs(a)>abs(b)){
         cout << ">";
     }
-    else if (pow(a,c)==pow(b,c)) {
+    else if (abs(a)<abs(b)) {
+        cout << "<";
+    }
+    else {
         cout << "=";
     }
+}
+else {
+     if (a>b){
+        cout << ">";
+    }
+    else if (a<b) {
+        cout << "<";
+    }
+    else {
+        cout << "=";
+    }
+}
 
     return 0;
 }
