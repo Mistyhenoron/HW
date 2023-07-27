a3243c15c479b912048a874e0fd58c357a0226be
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-07-27 09:50:34 +0500

    Create Dice33.cpp

---
+++ Homework2/Dice33.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int a,b;
+    cin >> a >> b;
+    if (b>a*6||b<a){
+        cout <<"No";
+    }
+    else {
+        cout << "Yes";
+    }
+
+    return 0;
+}
