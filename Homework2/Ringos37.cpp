cdcbe5186e3cf48bd140d80521c5b04b9b1b75f8
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-07-28 08:23:55 +0500

    Update Ringos37.cpp

--- Homework2/Ringos37.cpp
+++ Homework2/Ringos37.cpp
@@ -1,1 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+int n,x=0;
+cin >> n;
+int a[999];
+vector <int> cnt;
+for (int i =0;i<n;i++){
+    cin >> a[i];
+}
+for (int i = 0;i<n;i++){
+    for (int j = 1;j<n+1;j++){
+    if (a[j]-a[i]%200==0){
+        cnt.push_back(a[i]);}
+    }
+}
+for (int i=0;i<cnt.size();i++){
+    x++;
+}
+cout << (x*(x-1))/2;
+    return 0;
+}
+
 
