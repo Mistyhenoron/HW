c1dfce8beb4c6045ab9b426fff9ac4a016bd50b7
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-07-28 12:42:45 +0500

    Create Swap40.cpp

---
+++ Homework2/Swap40.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,x=0;
+    cin >> n;
+    int a [999];
+    for (int i = 0;i<n;i++){
+        cin >> a[i];
+    }
+    sort(a,a+n);
+    for (int i = 0; i <n;i++){
+        for (int j = 1;j<n;j++){
+            if (a[j]==a[i]){
+                x++;
+            }
+        }
+    }
+    cout << (n*(n-1))/2-(x*(x-1))/2;
+    return 0;
+}
