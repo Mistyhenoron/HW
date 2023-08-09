f2e6134afe7e616a6e06de5904b3615a6345b726
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-09 21:20:15 +0500

    Update Treschest55.cpp

--- Homework3/Treschest55.cpp
+++ Homework3/Treschest55.cpp
@@ -1,27 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-int n,lp=-1,rp=-1,star;
-cin >> n;
-string a[100];
-for (int i = 0;i<n;i++){
-    cin >> a[i];
-    if (a[i]=="|"){
-        if (lp==-1){
-            lp=i;
+    int n,lp=-1,rp=-1,star;
+    string a;
+    cin >> n >> a;
+    for (int i = 0;i<n;i++){
+        if (a[i]=='|'){
+            if (lp==-1){
+                lp=i;
+            }
+            else {
+                rp=i;
+            }
         }
-        else {
-            rp=i;
+        else if (a[i]=='*'){
+            star = i;
         }
     }
-    else if (a[i]=="*"){
-        star = i;
+    if (lp<star && star < rp){
+        cout << "in";
     }
-}
-if (lp<star && star < rp){
-    cout << "in";
-}
-else {
-cout << "out";}
+    else {
+        cout << "out";}
     return 0;
 }
