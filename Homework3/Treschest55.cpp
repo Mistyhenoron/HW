08ab743b749e4a30a972c59aea0739279c067fec
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-09 14:52:20 +0500

    Create Treschest55.cpp

---
+++ Homework3/Treschest55.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+int n,lp=-1,rp=-1,star;
+cin >> n;
+string a[100];
+for (int i = 0;i<n;i++){
+    cin >> a[i];
+    if (a[i]=="|"){
+        if (lp==-1){
+            lp=i;
+        }
+        else {
+            rp=i;
+        }
+    }
+    else if (a[i]=="*"){
+        star = i;
+    }
+}
+if (lp<star && star < rp){
+    cout << "in";
+}
+else {
+cout << "out";}
+    return 0;
+}
