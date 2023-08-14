129b89ce6d4099b82d2dab53cefadee51682a259
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-14 21:33:29 +0500

    Create Ancestor.cpp

---
+++ Homework4/Ancestor.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+int n;
+cin >> n;
+int a[999];
+int cnt = 0, act = n;
+for (int i = 1; i<n; i++){
+    cin >> a[i];
+}
+while (act!=1){
+    n++;
+    act = a[act];
+    act--;
+ }
+cout << n;
+    return 0;
+}
