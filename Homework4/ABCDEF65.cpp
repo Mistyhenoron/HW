ae4a16183f2724030c2541457523d55d6fedd40a
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-15 12:45:55 +0500

    Create ABCDEF65.cpp

---
+++ Homework4/ABCDEF65.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+long long a,b,c,d,e,f;
+cin >> a >> b >> c >> d >> e >> f;
+long long  x = ((a*b*c)-(d*e*f)) % 998244353;
+cout << x;
+    return 0;
+}
