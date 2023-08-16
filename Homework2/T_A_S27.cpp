930477ce12899f93964ee08a2290c4ed7b538de8
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-16 22:34:55 +0500

    Update T_A_S27.cpp

--- Homework2/T_A_S27.cpp
+++ Homework2/T_A_S27.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-int main()
-{
-    int A1,A2,A3;
+
+int main() {
+    int A1, A2, A3;
     cin >> A1 >> A2 >> A3;
-    if(A1-A2==A2-A3|| A2-A1==A1-A3|| A3-A2== A2-A1|| A3-A1==A1-A2|| A2-A3==A3-A1||A1-A3==A3-A2) {
+    if(A1 - A2 == A2 - A3 || A2 - A1 == A1 - A3 || A3 - A2 == A2 - A1 || A3 - A1 == A1 - A2 || A2 - A3 == A3 - A1 || A1 - A3 == A3 - A2) {
         cout << "Yes";
     }
     else {
