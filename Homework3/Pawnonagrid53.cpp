f96f0a6dd9724d46a8e460fa8986c5815b76d3f6
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-16 22:45:42 +0500

    Update Pawnonagrid53.cpp

--- Homework3/Pawnonagrid53.cpp
+++ Homework3/Pawnonagrid53.cpp
@@ -1,21 +1,23 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-int main(){
-int h,w,x=0;
-cin >> h >> w;
-string a[100][100];
-for (int i = 0;i<h;i++){
-    for (int j = 0;j<w;j++){
-        cin >> a[i][j];
-    }
-}
-for (int i = 0;i<h;i++){
-    for (int j = 0;j<w;j++){
-        if (a[i][j]=="#"){
-            x++;
+
+int main() {
+    int h, w, x = 0;
+    cin >> h >> w;
+    string a[100][100];
+    for (int i = 0; i < h; i++){
+        for (int j = 0; j < w; j++){
+            cin >> a[i][j];
         }
     }
-}
-cout << x;
-    return 0;
+    for (int i = 0; i < h; i++){
+        for (int j = 0; j < w; j++){
+            if (a[i][j] == "#"){
+                x++;
+            }
+        }
+    }
+    cout << x;
+     return 0;
 }
