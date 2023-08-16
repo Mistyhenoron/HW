3f01f392bb1c24e62498103a7e0adbc9946ed4a3
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-16 22:28:30 +0500

    Update Canyoubuy35.cpp

--- Homework2/Canyoubuy35.cpp
+++ Homework2/Canyoubuy35.cpp
@@ -1,19 +1,21 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-int x,n,s=0;
+
+int x,n,s = 0;
 int a[444];
 int main() {
     cin >> n >> x;
-    for (int i=1;i<n+1;i++){
+    for (int i = 1; i < n + 1; i++){
         cin >> a[i];
     }
-    for (int i = 1;i<n+1;i++){
-        s+=a[i];
-        if (i%2==0){
+    for (int i = 1; i < n + 1; i++){
+        s += a[i];
+        if (i%2 == 0){
             s--;
         }
     }
-    if (s<=x) {
+    if (s <= x) {
         cout << "Yes";
     }
     else {
