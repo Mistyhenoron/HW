20aaf7e01f908c10c23594cb8beadf823b7051b5
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-16 22:46:35 +0500

    Update Treschest55.cpp

--- Homework3/Treschest55.cpp
+++ Homework3/Treschest55.cpp
@@ -1,19 +1,21 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-int main(){
-    int n,lp=-1,rp=-1,star;
+
+int main() {
+    int n, lp=-1, rp=-1, star;
     string a;
     cin >> n >> a;
-    for (int i = 0;i<n;i++){
-        if (a[i]=='|'){
-            if (lp==-1){
-                lp=i;
+    for (int i = 0; i < n; i++){
+        if (a[i] == '|'){
+            if (lp == -1){
+                lp = i;
             }
             else {
-                rp=i;
+                rp = i;
             }
         }
-        else if (a[i]=='*'){
+        else if (a[i] == '*'){
             star = i;
         }
     }
