24bb49015fb701f169c23db8bc9be757e516ba7a
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-16 22:47:52 +0500

    Update Ancestor60.cpp

--- Homework4/Ancestor60.cpp
+++ Homework4/Ancestor60.cpp
@@ -2,19 +2,19 @@
 
 using namespace std;
 
-int main(){
-int n;
-cin >> n;
-int a[999];
-int cnt = 0, act = n;
-for (int i = 1; i<n; i++){
-    cin >> a[i];
-}
-while (act!=1){
-    n++;
-    act = a[act];
-    act--;
- }
-cout << n;
-    return 0;
+int main() {
+    int n;
+    cin >> n;
+    int a[999];
+    int cnt = 0, act = n;
+    for (int i = 1; i < n; i++){
+        cin >> a[i];
+    }
+    while (act != 1){
+        n++;
+        act = a[act];
+        act--;
+     }
+    cout << n;
+     return 0;
 }
