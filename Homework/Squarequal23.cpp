1ad5e28c077cf9753a8cbaa1f1029019d37f7f1b
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-16 22:25:31 +0500

    Update Squarequal23.cpp

--- Homework/Squarequal23.cpp
+++ Homework/Squarequal23.cpp
@@ -1,21 +1,22 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-bool disk(double a, double b){
+
+bool disk(double a, double b) {
     if(abs(a-b)<1e-9){
         return 1;
     }
     return 0;
 }
-int main(){
+int main() {
     double a,b,c;
     cin >> a >> b >> c;
-    double d=b*b - 4*a*c;
+    double d = b*b - 4*a*c;
     if (disk(d,0)){
             cout << setprecision(9) << fixed <<  -b/(2*a);
-
     }
 
-    else if (d>0){
+    else if (d>0) {
         double sq = sqrt(abs(d));
         cout << setprecision(9) << fixed<<  (-b + sq)/(2*a) <<"\n" << (-b - sq)/(2*a);
     }
