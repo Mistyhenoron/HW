775765d38e17ae045b97efc07e419189a876df70
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-16 22:31:12 +0500

    Update Percheck31.cpp

--- Homework2/Percheck31.cpp
+++ Homework2/Percheck31.cpp
@@ -1,19 +1,21 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+
 int main() {
-    int n,x=0;
+    int n, x = 0;
     cin >> n;
     int a[n];
-    for (int i=0;i<n;i++){
+    for (int i = 0; i < n; i++){
         cin >> a[i];
     }
     sort(a,a+n);
-    for (int i = 0;i<n;i++){
-        if (a[i+1]-a[i]==1){
+    for (int i = 0; i < n; i++){
+        if (a[i + 1] - a[i] == 1){
             x++;
         }
     }
-    if (x==n-1){
+    if (x == n - 1){
         cout << "Yes";
     }
     else {
