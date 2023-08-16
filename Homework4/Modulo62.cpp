f3bbb4bc2afa4e3c45e7cb9905fd5550f24d8109
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-16 22:48:09 +0500

    Update Modulo62.cpp

--- Homework4/Modulo62.cpp
+++ Homework4/Modulo62.cpp
@@ -1,12 +1,14 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-int main(){
-long long n;
-cin >> n;
-long long x = n % 998244353;
-if (x<0){
-    x = x + 998244353;
-}
-cout << x;
-    return 0;
+
+int main() {
+    long long n;
+    cin >> n;
+    long long x = n % 998244353;
+    if (x<0){
+        x = x + 998244353;
     }
+    cout << x;
+        return 0;
+}
