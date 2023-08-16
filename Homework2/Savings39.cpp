072934debc3a4584661bca85a55cf4cb2ef6568e
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-16 22:33:05 +0500

    Update Savings39.cpp

--- Homework2/Savings39.cpp
+++ Homework2/Savings39.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+
 using namespace std;
+
 int main() {
-    int n,d=0,s=0;
+    int n, d = 0, s = 0;
     cin >> n;
-    while(s<n){
+    while(s < n){
         d++;
-        s+=d;
+        s += d;
     }
     cout << d;
     return 0;
