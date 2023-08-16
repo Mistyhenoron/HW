385cd80cebf062023c091ec42c2ab1610dbc7fdf
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-16 22:42:41 +0500

    Update Fullhouse48.cpp

--- Homework3/Fullhouse48.cpp
+++ Homework3/Fullhouse48.cpp
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int main() {
     int a[5];
-    for (int i = 0;i<5;i++){
+    for (int i = 0; i < 5; i++){
         cin >> a[i];
     }
-    sort(a,a+5);
-    if((a[0]==a[1]&& a[1]==a[2]&& a[3]==a[4])||a[0]==a[1]&& a[2]==a[3]&& a[3]==a[4]) {
+    sort(a, a + 5);
+    if((a[0] == a[1] && a[1] == a[2] && a[3] == a[4]) || a[0] == a[1] && a[2] == a[3] && a[3] == a[4]) {
         cout << "Yes";
     }
     else {
