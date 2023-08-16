74478ca084ebc458f0ba42137773a5d4a4f8f1f2
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-16 22:22:26 +0500

    Update Diff11.cpp

--- Homework/Diff11.cpp
+++ Homework/Diff11.cpp
@@ -2,10 +2,10 @@
 
 using namespace std;
 
-int Diff11() {
+int main() {
     int h1,m1,s1,h2,m2,s2;
     cin >> h1 >> m1 >> s1 >> h2 >> m2 >> s2;
-    cout << (h2*60*60 - h1*60*60) + (m2*60-m1*60) + (s2-s1);
+    cout << (h2*60*60 - h1*60*60) + (m2*60 - m1*60) + (s2 - s1);
     return 0;
 }
 
