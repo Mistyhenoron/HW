54b15e7900f508ba9495a69b629c6d5ec9fdbff1
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-16 22:29:08 +0500

    Update Dice33.cpp

--- Homework2/Dice33.cpp
+++ Homework2/Dice33.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+
 int main() {
     int a,b;
     cin >> a >> b;
-    if (b>a*6||b<a){
+    if (b > a*6 || b < a){
         cout <<"No";
     }
     else {
