a8aef624c273702360bc12b61b24ea7338868931
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-16 22:27:38 +0500

    Update Cabbages36.cpp

--- Homework2/Cabbages36.cpp
+++ Homework2/Cabbages36.cpp
@@ -1,10 +1,12 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+
 int main(){
     int a,x,y,n;
     cin >> n >> a >> x >> y;
-    if(a<n){
-    cout << (n-(n-a))* x + (n-a)*y;}
+    if(a < n){
+    cout << (n - (n - a))* x + (n - a)*y;}
     else {
         cout << n*x;
     }
