e22e5433a28212191c12f68abb5b84b65a1841a0
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-16 22:54:00 +0500

    Update Rotation79.cpp

--- Homework5/Rotation79.cpp
+++ Homework5/Rotation79.cpp
@@ -2,28 +2,28 @@
 
 using namespace std;
 
-int main(){
-string s;
-int a[100][2];
-int n,q;
-cin >> n >> q;
-for (int i = 0; i<n; i++){
-    cin >> s[i];
-}
-for (int i = 0; i<n; i++){
-    for (int j = 0; j<n; j++){
-        cin >> a[i][j];
-        if (a[i][1]==1){
-            for (int i = 0; i < a[i][2];i++){
-                s[0] == s[n];
-                delete s[i];
-                else if (a[i][1]==2){
-                    cout << s[a[i][1]];
+int main() {
+    string s;
+    int a[100][2];
+    int n, q;
+    cin >> n >> q;
+    for (int i = 0; i < n; i++){
+        cin >> s[i];
+    }
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            cin >> a[i][j];
+            if (a[i][1] == 1){
+                for (int i = 0; i < a[i][2]; i++){
+                    s[0] == s[n];
+                    delete s[i];
+                    else if (a[i][1] == 2){
+                        cout << s[a[i][1]];
+                    }
+    
                 }
-
             }
         }
     }
-}
-    return 0;
+        return 0;
 }
