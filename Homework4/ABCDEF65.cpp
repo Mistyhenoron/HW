01d5fa69d0a0b9f39573f4dcca78f9970c517484
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-18 22:30:12 +0500

    Update ABCDEF65.cpp

--- Homework4/ABCDEF65.cpp
+++ Homework4/ABCDEF65.cpp
@@ -1,11 +1,17 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
+const int mod = 998244353;
 int main() {
     long long a, b, c, d, e, f;
-    cin >> a >> b >> c >> d >> e >> f;
-    long long  x = ((a*b*c) - (d*e*f)) % 998244353;
-    cout << x;
-        return 0;
+    cin >> a >> b >> c >> d >> f;
+    a = a % mod;
+    b = b % mod;
+    c = c % mod;
+    d = d % mod;
+    e = e % mod;
+    f = f % mod;
+    long long l = ((a*b % mod)*c) % mod;
+    long long r = ((d*e % mod)*f) % mod;
+    cout << (l - r + mod) % mod;
+    return 0;
 }
