be20bf8bbc84c4ced28b1417f33572a1ed9cd718
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-20 00:06:44 +0500

    Update QualB72.cpp

--- Homework5/QualB72.cpp
+++ Homework5/QualB72.cpp
@@ -2,22 +2,18 @@
 
 using namespace std;
 
+int n, k;
+string s;
+
 int main() {
-    int n, k;
-    cin >> n >> k;
-    string s;
-    for (int i = 0; i < n; i++){
-        cin >> s[i];
-    }
+    cin >> n >> k >> s;
     for (int i = 0; i < n; i++){
             if (k!=0 && s[i]=='o'){
                 cout << 'o';
                 k--;
-            }
-            else {
+            } else {
                 cout << 'x';
             }
-    
     }
         return 0;
 }
