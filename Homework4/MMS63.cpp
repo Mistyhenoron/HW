6164af58caf7d787cd86148214886a27b9fba2d3
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-19 23:00:59 +0500

    Create MMS63.cpp

---
+++ Homework4/MMS63.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, q;
+const int N = 2e5 + 4;
+vector <int> a[N];
+
+int main() {
+    cin >> n >> q;
+    for (int i = 1; i <=n; i++) {
+        int l;
+        cin >> l;
+        a[i].resize(l);
+        for (int j = 1; j <= l; j++){
+            cin >> a[i][j];
+        }
+
+    }
+    for (int i = 1; i <= q; i++) {
+        int s, t;
+        cin >> s >> t;
+        cout << a[s][t ] << "\n";
+    }
+  
+    return 0;
+}
