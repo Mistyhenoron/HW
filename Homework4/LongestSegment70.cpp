1b8d8a82100644686395d24f4aefb6bc5ba2919f
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-25 20:11:21 +0500

    Update LongestSegment70.cpp

--- Homework4/LongestSegment70.cpp
+++ Homework4/LongestSegment70.cpp
@@ -2,8 +2,20 @@
 
 using namespace std;
 
-int n, a[444][444];
+const int N = 104;
+int n, a[N][2];
 double maxx = 0;
+
+double d(int i, int j) {
+    double xi = a[i][0];
+    double yi = a[i][1];
+    double xj = a[j][0];
+    double yj = a[j][1];
+    double dx = abs(xi - xj);
+    double dy = abs(yi - yj);
+    return sqrt (dx*dx + dy*dy);
+}
+
 int main() {
     cin >> n;
     for (int i = 0; i < n; i++) {
@@ -12,10 +24,9 @@ int main() {
         }
     }
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < 2; j++) {
-            if (sqrt((abs(a[i][j] - a[i+1][j]))*abs((a[i][j] - a[i+1][j])) + (abs(a[i][j+1] - a[i+1][j+1]))*(abs(a[i][j+1] - a[i+1][j+1]))) > maxx) {
-               maxx = sqrt((abs(a[i][j] - a[i+1][j]))*abs((a[i][j] - a[i+1][j])) + (abs(a[i][j+1] - a[i+1][j+1]))*(abs(a[i][j+1] - a[i+1][j+1])));
-            }
+        for (int j = 0; j < n; j++) {
+            double dist = d(i, j);
+            maxx = max(maxx, dist);
         }
     }
     cout << fixed << setprecision(15) << maxx;
