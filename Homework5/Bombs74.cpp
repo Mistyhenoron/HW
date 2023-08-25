318252f5386752101ead1fdb0c83c4e22c10b846
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-25 21:14:58 +0500

    Update Bombs74.cpp

--- Homework5/Bombs74.cpp
+++ Homework5/Bombs74.cpp
@@ -1,73 +1,38 @@
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int r, c, z;
-char a[100][100];
-int mn() {
+const int R = 24, C = 24;
+int r, c;
+char a[R][C], b[R][C];
+int main() {
     cin >> r >> c;
     for (int i = 0; i < r; i++) {
         for (int j = 0; j < c; j++) {
             cin >>  a[i][j];
+            b[i][j] = a[i][j];
         }
     }
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++) {
-            if (a[i][j] == 1) {
-                a[i][j] == '.';
-                a[i-1][j] == '.';
-                a[i+1][j] == '.';
-                a[i][j-1] == '.';
-                a[i][j+1] == '.';
-            }
-            else if (a[i][j] == 2) {
-                a[i][j] == '.';
-                a[i-1][j] == '.';
-                a[i+1][j] == '.';
-                a[i][j-1] == '.';
-                a[i][j+1] == '.';
-                a[i-2][j] == '.';
-                a[i+2][j] == '.';
-                a[i][j+2] == '.';
-                a[i][j-2] == '.';
-                a[i-1][j-1] == '.';
-                a[i+1][j-1] == '.';
-                a[i+1][j+1] == '.';
-                a[i-1][j+1] == '.';
-            }
-            else if (a[i][j] == 3) {
-                a[i][j] == '.';
-                a[i-1][j] == '.';
-                a[i+1][j] == '.';
-                a[i][j-1] == '.';
-                a[i][j+1] == '.';
-                a[i-2][j] == '.';
-                a[i+2][j] == '.';
-                a[i][j+2] == '.';
-                a[i][j-2] == '.';
-                a[i-1][j-1] == '.';
-                a[i+1][j-1] == '.';
-                a[i+1][j+1] == '.';
-                a[i-1][j+1] == '.';
-                a[i+3][j] == '.';
-                a[i-3][j] == '.';
-                a[i][j-3] == '.';
-                a[i][j+3] == '.';
-                a[i-1][j-2] == '.';
-                a[i-2][j-1] == '.';
-                a[i-2][j+1] == '.';
-                a[i-1][j+2] == '.';
-                a[i+1][j-2] == '.';
-                a[i+2][j-1] == '.';
-                a[i+2][j+1] == '.';
-                a[i+1][j+2] == '.';
+for (int i = 0; i < r; i++) {
+    for (int j = 0; j < c; j++) {
+        if (a[i][j] == '#' && a[i][j] == '.') {
+                 continue;
+        }
+        int d  = a[i][j] - '0';
+            for(int  y = 0; y < r; y++) {
+                for (int x = 0; x < c; x++) {
+                    if (abs(y - i) + abs(x - j) <= d) {
+                        b[y][x] == '.';
+                    }
+                }
             }
         }
     }
+
+
     for (int i = 0; i < r; i++) {
         for (int j = 0; j < c; j++) {
-            cout << a[i][j];
+            cout << b[i][j];
         }
         cout << "\n";
     }
