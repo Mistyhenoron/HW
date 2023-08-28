86170a3a597e82d2a7859e1046205f97e670ee54
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-28 14:51:38 +0500

    Update Mex91.cpp

--- Homework7/Mex91.cpp
+++ Homework7/Mex91.cpp
@@ -3,27 +3,25 @@
 using namespace std;
 
 const int N = 2004;
-int a[N], n, minn = 2004, maxx = -1, x;
+
+int a[N], n, ecount;
 
 int main() {
     cin >> n;
     for (int i = 0; i < n; i++) {
         cin >> a[i];
-        if (a[i] > 0 && a[i] < minn) {
-            minn = a[i];
-        }
-        if (a[i] > 0 && a[i] > maxx) {
-            maxx = a[i];
-        }
     }
     for (int i = 0; i < 2004; i++) {
         for (int j = 0; j < n; j++) {
-            if (i != a[j] && i <= maxx) {
-                x  == i;
-            }
+                if (a[j] == i) {
+                    ecount++;
+                }
         }
+                if (ecount == 0) {
+                    cout << i;
+                    break;
+                }
+                ecount = 0;
     }
-
-    cout << x;
     return 0;
 }
