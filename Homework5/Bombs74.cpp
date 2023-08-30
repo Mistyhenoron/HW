c4cd8181a8abff4a94fbb0fa9e8d770dd77a0ec7
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-08-30 23:23:37 +0500

    Update Bombs74.cpp

--- Homework5/Bombs74.cpp
+++ Homework5/Bombs74.cpp
@@ -2,9 +2,9 @@
 
 using namespace std;
 
-const int R = 24, C = 24;
+const int N = 24;
 int r, c;
-char a[R][C], b[R][C];
+char a[N][N], b[N][N];
 int main() {
     cin >> r >> c;
     for (int i = 0; i < r; i++) {
@@ -22,14 +22,12 @@ for (int i = 0; i < r; i++) {
             for(int  y = 0; y < r; y++) {
                 for (int x = 0; x < c; x++) {
                     if (abs(y - i) + abs(x - j) <= d) {
-                        b[y][x] == '.';
+                        b[y][x] = '.';
                     }
                 }
             }
         }
     }
-
-
     for (int i = 0; i < r; i++) {
         for (int j = 0; j < c; j++) {
             cout << b[i][j];
