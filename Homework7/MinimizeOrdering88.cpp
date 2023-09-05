68869f06e8f7eb9dfca6966b4aa5744bd9cb43ce
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-09-05 21:45:48 +0500

    Create MinimizeOrdering88.cpp

---
+++ Homework7/MinimizeOrdering88.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 40;
+string s;
+int cnt[N]
+
+int main() {
+
+    cin >> s;
+    for (int i = 0; i < s.size(); i++) {
+        cnt[s[i]]++;
+    }
+    for (int с = 'a'; с != 'z'; с++) ????????}
+        for (int i = 0; i < cnt[с]) {
+            cout << c;
+        }
+    }
+
+    return 0;
+}
