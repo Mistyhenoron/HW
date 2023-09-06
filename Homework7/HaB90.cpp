57e238fad291bfeda83aa0570502a89a67865e39
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-09-06 13:57:36 +0500

    Update HaB90.cpp

--- Homework7/HaB90.cpp
+++ Homework7/HaB90.cpp
@@ -3,26 +3,22 @@
 using namespace std;
 
 const int N = 1004;
-int n, a[N], b[N], m[N][N], x, y;
+int n, a[N], b[N], x, y;
 
 int main() {
     cin >> n;
     for (int i = 0; i < n; i++) {
         cin >> a[i];
-        m[0][i] == a[i];
     }
     for (int i = 0; i < n; i++) {
         cin >> b[i];
-        m[1][i] == b[i];
     }
-    for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (m[i+1][j] == m[i][j]) {
-            x++;
-            break;
+            if (a[j] == b[j]) {
+                x++;
+                break;
             }
         }
-    }
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             if (b[j] == a[i]) {
