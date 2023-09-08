88bcb2385c459de47ddf93e0dfb8f38204de3700
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-09-08 23:21:26 +0500

    Update CaesarCipher69.cpp

--- Homework4/CaesarCipher69.cpp
+++ Homework4/CaesarCipher69.cpp
@@ -4,15 +4,22 @@ using namespace std;
 
 string s, t;
 
+int cnt(char a, char b) {
+    if (a < b) {
+        return b - a;
+    }
+        return 26 - (a - b);
+}
+
 int main() {
     cin >> s >> t;
-    for (int i = 0; i < s.size(); i++) {
-        if (s[i] - t[i] == s[i+1] - t[i+1]) {
+    for (int i = 0; i + 1 < s.size(); i++) {
+        if (cnt(s[i], t[i]) ==  cnt(s[i+1], t[i+1])) {
             continue;
         }
         else {
-            break;
             cout << "No";
+            return 0;
         }
     }
     cout << "Yes";
