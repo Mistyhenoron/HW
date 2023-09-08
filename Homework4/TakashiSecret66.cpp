00b32a4bb6a35415e3ff6dab788e2435ca5ade2c
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-09-08 14:57:34 +0500

    Update TakashiSecret66.cpp

--- Homework4/TakashiSecret66.cpp
+++ Homework4/TakashiSecret66.cpp
@@ -5,32 +5,26 @@ using namespace std;
 const int N = 1e5 + 4;
 
 int n, x, a[N], j, z, m, y;
- bool b[N];
+bool b[N];
 
 int main() {
-
     cin >> n >> x;
     for (int i = 1; i <= n; i++) {
         cin >> a[i];
     }
-    m = a[x];
-    b[m] = 1;
-    while (n!=0) {
-        y = a[m];
-        j++;
-        n--;
-        if (b[y] == 1) {
+    int s = x;
+    while (true) {
+        b[s] = 1;
+        int nxt = a[s];
+        if (b[nxt] = 1) {
             break;
         }
-        b[y] = 1;
-        m = a[y];
-        j++;
-        n--;
-        if (b[m] == 1) {
-            break;
-        }
-        b[m] = 1;
+        s = nxt;
     }
-    cout << j;
+    int res = 0;
+    for (int i = 1; i <= n; ++i) {
+        res += b[i];
+    }
+    cout << res;
     return 0;
 }
