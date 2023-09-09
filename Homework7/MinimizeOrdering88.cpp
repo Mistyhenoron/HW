fdac12151c81f409aa155a845bea3054c3a2db8d
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-09-09 11:22:06 +0500

    Update MinimizeOrdering88.cpp

--- Homework7/MinimizeOrdering88.cpp
+++ Homework7/MinimizeOrdering88.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 const int N = 40;
 string s;
-int cnt[N]
+int cnt[N];
 
 int main() {
 
@@ -12,11 +12,11 @@ int main() {
     for (int i = 0; i < s.size(); i++) {
         cnt[s[i]]++;
     }
-    for (int с = 'a'; с != 'z'; с++) ????????}
-        for (int i = 0; i < cnt[с]) {
-            cout << c;
+    for (int c = 'a'; c <= 'z'; c++){
+        for (int i = 0; i < cnt[c]; i++) {
+            cout << (char)c;
         }
     }
-
     return 0;
 }
+
