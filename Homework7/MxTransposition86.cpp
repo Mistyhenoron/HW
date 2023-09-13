cc8b612bcc4ff63a74b9482f2f76640880c06df0
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-09-13 21:37:29 +0500

    Update MxTransposition86.cpp

--- Homework7/MxTransposition86.cpp
+++ Homework7/MxTransposition86.cpp
@@ -3,10 +3,13 @@
 using namespace std;
 
 const long long N = 10000;
-long long h, w, a[N][N], b[N][N];
-
+long long h, w;
+vector<vector<int>> a, b;
 int main() {
     cin >> h >> w;
+    a.resize(h, vector<int>(w));
+    b.resize(w, vector<int>(h));
+
     for (int i = 0; i < h; i++) {
         for (int j = 0; j < w; j++) {
             cin >> a[i][j];
