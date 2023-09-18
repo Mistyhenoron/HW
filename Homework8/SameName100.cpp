cdff443d23108e6bef0a534c518b226ccc3af534
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-09-18 17:00:30 +0500

    Create SameName100.cpp

---
+++ Homework8/SameName100.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 1004;
+int n;
+string s[N], t[N];
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> s[i] >> t[i];
+       }
+       for (int i = 0; i < n; i++) {
+         for (int j = 0; j < n; j++) {
+            if (s[i] == s[j] && t[i] == t[j]) {
+                cout << "Yes";
+                return 0;
+                }
+            }
+       }
+    cout << "No";
+    return 0;
+    }
