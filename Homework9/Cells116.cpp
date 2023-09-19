c2ba43186bf7ec23e03a840e2c8984c94ad96dc7
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-09-19 07:54:55 +0500

    Update Cells116.cpp

--- Homework9/Cells116.cpp
+++ Homework9/Cells116.cpp
@@ -7,41 +7,16 @@ char b;
 
 int main() {
     cin >> b >> a;
-    if (b == 'A') {
-        c =  1;
-    }
-    else if (b == 'B') {
-        c = 2;
-    }
-    else if (b == 'C') {
-        c = 3;
-    }
-    else if (b == 'D') {
-        c = 4;
-    }
-    else if (b == 'E') {
-        c = 5;
-    }
-    else if (b == 'F') {
-        c = 6;
-    }
-    else if (b == 'G') {
-        c = 7;
-    }
-    else if (b == 'H') {
-        c = 8;
-    }
-    if (c%2 == 0 && a%2 == 0) {
+    c = (b - 'A')%2;
+    a %= 2;
+
+    if ((a == 0 && c == 1) || (a == 1 && c == 0)) {
         cout << "BLACK";
     }
-    else if (c%2 == 0 && a%2 != 0) {
+
+    else {
         cout << "WHITE";
-    }
-    else if (c%2 != 0 && a%2 == 0) {
-        cout << "WHITE";
-    }
-    else if (c%2 != 0 && a%2 != 0) {
-        cout << "BLACK";
     }
     return 0;
 }
+
