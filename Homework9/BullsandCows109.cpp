744f8c43cc2a8f587ec0f3c31ea3329394a5a3e3
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-09-19 07:44:37 +0500

    Update BullsandCows109.cpp

--- Homework9/BullsandCows109.cpp
+++ Homework9/BullsandCows109.cpp
@@ -3,28 +3,23 @@
 using namespace std;
 
 const int N = 1004;
-int bulls, cows, s21, s22, s23, s24, s2, s11, s12, s13, s14, s1;
-int a[N];
-vector <int> b;
-vector <int> c;
+int bulls, cows, a[N];
+vector<int> b, c;
+string s1, s2;
+
+vector<int> conv(string s) {
+    vector<int> res;
+    for (int i = 0; i < s.size(); i++) {
+            res.push_back(s[i] - '0');
+    }
+    return res;
+}
+
 int main() {
     cin >> s1 >> s2;
-    s21 = s2/1000;
-    s22 = s2/100%10;
-    s23 = s2/10%10;
-    s24 = s2%10;
-    b.push_back(s21);
-    b.push_back(s22);
-    b.push_back(s23);
-    b.push_back(s24);
-    s11 = s1/1000;
-    s12 = s1/100%10;
-    s13 = s1/10%10;
-    s14 = s1%10;
-    c.push_back(s11);
-    c.push_back(s12);
-    c.push_back(s13);
-    c.push_back(s14);
+    b = conv(s1);
+    c = conv(s2);
+
     for (int i = 0; i < 4; i++) {
         if (c[i] == b[i]) {
             bulls++;
