2ab2f63cc99acaac1058dc5b18360e976fe8cabe
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-09-20 22:39:57 +0500

    Update Painter110.cpp

--- Homework9/Painter110.cpp
+++ Homework9/Painter110.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 const int N = 104;
 
-int h, w, n, a[N][N];
+int h, w, n, a[N][N], res;
 
 int main() {
     for (int i = 0; i < h; i++) {
@@ -16,17 +16,17 @@ int main() {
     for (int i = 0; i < n; i++) {
         int x1, y1, x2, y2;
         cin >> x1 >> y1 >> x2 >> y2;
-        for (int i = x1; i != x2-1; i++) {
-            for (int j = y1; i != y2-1; i++) {
+        for (int i = x1; i <= x2-1; i++) {
+            for (int j = y1; j <= y2-1; j++) {
                 a[i][j] = 1;
             }
         }
     }
     for (int i = 0; i < h; i++) {
         for (int j = 0; j < w; j++) {
-            cout << a[i][j];
+            res += (a[i][j] == 0);
         }
     }
-
+    cout << res;
     return 0;
 }
