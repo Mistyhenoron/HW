b71b2715c0e6dc887b69cc534ac7174b2f264fd5
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-09-20 22:32:13 +0500

    Update SameName100.cpp

--- Homework8/SameName100.cpp
+++ Homework8/SameName100.cpp
@@ -3,13 +3,16 @@
 using namespace std;
 
 const int N = 1004;
+
 int n;
+
 string s[N], t[N];
 int main() {
     cin >> n;
     for (int i = 0; i < n; i++) {
         cin >> s[i] >> t[i];
        }
+    
        for (int i = 0; i < n; i++) {
          for (int j = i + 1; j < n; j++) {
             if (s[i] == s[j] && t[i] == t[j]) {
