e56e1ce28ca7c794b60e96abaf7578428c486b21
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-09-30 14:13:30 +0500

    Update Stepstrok107.cpp

--- Homework9/Stepstrok107.cpp
+++ Homework9/Stepstrok107.cpp
@@ -2,26 +2,32 @@
 
 using namespace std;
 
-string s, call;
+string s, call, t;
 int k;
 
 int main() {
     cin >> s >> k;
     if (k > 0) {
         for (int i = 0; i < k; i++) {
-            cout << s;
-            return 0;
+            t += s;
+            if (t.size() > 1023) {
+                t.resize(1023);
+                break;
+            }
         }
+        cout << t;
+        return 0;
     }
-    else if (k < 0 && s.size()%abs(k) == 0 && s.size()/abs(k) != 1) {
-        k = abs(k);
-        for (int i = 0; i < s.size()/k; i++) {
-            call += s[i];
-        }
-    }
-       else {
+    if (s.size()%k != 0) {
         cout << "No solution";
     }
-    cout << call;
+    else {
+        for (int i = 1; i < s/k; i++) {
+            for (int j = i; j < s.size(); j+= s/k) {
+                ????????????
+                }
+            }
+        }
+    }
   return 0;
 }
