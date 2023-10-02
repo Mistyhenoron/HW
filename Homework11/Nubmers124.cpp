e206956f640fae8eff1dd647cafd73bc8fe41cd6
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-10-02 17:21:07 +0500

    Create Nubmers124.cpp

---
+++ Homework11/Nubmers124.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string s, x, y, xy;
+int maxx = -1e8;
+
+int pmsum(string x) {
+    int itog;
+    for (int i = 0; i < x.size(); i++) {
+        if ((i - '0') % 2 == 0) {
+            itog += (x[i] - '0');
+        }
+        else {
+            itog -= (x[i] - '0');
+        }
+    }
+    return itog;
+}
+
+int main() {
+    cin >> s;
+    for (int i = 0; i < s.size(); i++) {
+      x =  s.substr(i+1, s.size());
+      y =  s.substr(0, i);
+      xy = y + x;
+      int temp = pmsum(xy);
+      if (temp > maxx) {
+      maxx = temp;
+      }
+    }
+    cout << maxx;
+    return 0;
+}
