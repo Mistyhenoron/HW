15e5a52b9e366026fb99bca47da802b9715d36aa
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-10-05 21:22:41 +0500

    Create SSA150.cpp

---
+++ Homework13/SSA150.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a, b, c;
+
+int main() {
+    cin >> a;
+    if (a % 2 == 0) {
+        cout << a + 1;
+    }
+    else {
+        cout << a - 1;
+    }
+    return 0;
+}
