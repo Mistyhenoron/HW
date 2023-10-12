bdc1fcc6a1f885ffa9c06b9ecd728dcbb5d9aa0a
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-10-12 22:49:50 +0500

    Update Scuza142.cpp

--- Homework13/Scuza142.cpp
+++ Homework13/Scuza142.cpp
@@ -6,6 +6,21 @@ const int N = 1e5 + 7;
 
 long long t, nm[N], qm[N];
 
+int bs(int a[N], int n) {
+    sort(a[0], a[n-1]);
+    int l = 0, r = n - 1;
+    while (l + 1 < r) {
+        int m = (l + r)/2;
+        if (a[m] >= n) {
+            r = m;
+        }
+        else {
+            l = m;
+        }
+    }
+    return r;
+}
+
 int main() {
     cin >> t;
     for (int i = 0; i < t; i++) {
@@ -19,13 +34,9 @@ int main() {
         }
         for (int i = 0; i < q; i++) {
                 long long ans = 0;
-            for (int j = 0; j < n; j++) {
-                if (qm[i] >= nm[j]) {
+                int g = bs(nm, n);
+            for (int j = 0; j < g; j++) {
                     ans += nm[j];
-                }
-                else {
-                    break;
-                }
             }
         cout << ans << " ";
         }
