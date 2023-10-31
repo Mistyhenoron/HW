d4cf0a679ec2f43c5aac125567eebe8d4622be1b
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-11-01 00:42:43 +0500

    Create SSC152.cpp

---
+++ Homework13/SSC152.cpp
@@ -0,0 +1,17 @@
+#inlcude <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 1004;
+int n, a[N];
+
+int main() {
+    cin >> n;
+    while (n--) {
+        cin >> ai >> bi >> pi >> qi >> ri;
+        int t = -bi/2*ai
+        // А где взять l и r?;
+    }
+  
+    return 0;
+}
