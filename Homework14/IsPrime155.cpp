a9abc0114896567b6111f20f47322d8adfc11eb6
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-11-04 02:09:16 +0500

    Create IsPrime155.cpp

---
+++ Homework14/IsPrime155.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+void isPrime(int n) {
+    for (int i = 2; i*i <= n; i++) {
+        if (n%i == 0) {
+            cout << "composite";
+            return;
+        }
+    }
+    cout << "prime";
+return;
+
+}
+
+int main() {
+    cin >> n;
+    isPrime(n);
+    return 0;
+}
