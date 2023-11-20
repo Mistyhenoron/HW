7ce6f15151e58f55eb3e80a18af18f2893bc1d48
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-11-20 17:30:11 +0500

    Create Pridp169.cpp

---
+++ Homework14/Pridp169.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, k;
+vector <int> a;
+int bs(int x) {
+    int l = 0, r = n - 1;
+   while (l + 1 < r) {
+        int mid = (r + l) / 2;
+        if (a[mid] > x)
+            r = mid;
+        else
+            l = mid;
+    }
+    return r;
+}
+int main() {
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        int g;
+        cin >> g;
+        a.push_back(g);
+    }
+    while (k--) {
+        int temp;
+        cin >> temp;
+        int f;
+        f = bs(temp);
+        if (f == temp) {
+            cout << temp;
+            return 0;
+        }
+        else {
+                /*
+           auto x1 = lower_bound(a.begin(), a.end(), temp);
+           auto x2 = upper_bound(a.begin(), a.end(), temp);
+            if (abs(a[x1] - temp) < abs(a[x2] - temp)) {
+                cout << a[x1];
+            }
+            else {
+                a[x2];
+            }
+            */ // тут проблемы
+        }
+    }
+    return 0;
+}
