26c71a73c75390c48e8cf0f0471380137a127580
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-11-28 23:25:11 +0500

    Create Matrix167.cpp

---
+++ Homework14/Matrix167.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 1004;
+int n, m, a[N][N],minn[N], maxx[N], mini = 99999, maxi = -999999, cnt;
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> a[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (a[i][j] <= mini) {
+                minn[i] = a[i][j];
+            }
+        }
+    }
+    for (int j = 0; j < m; j++) {
+        for (int i = 0; i < n; i++) {
+            if (a[j][i] >= maxi) {
+                maxx[j] = a[j][i];
+            }
+        }
+    }
+    for (int i = 0;  i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (a[i][j] == minn[i]  && a[i][j] == maxx[j]) {
+                cnt++;
+            }
+        }
+    }
+    cout << cnt;
+    return 0;
+}
