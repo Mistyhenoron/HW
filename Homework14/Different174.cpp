96d89cee2b34854db1dde95e0e3ee11fd66b6ff8
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2023-12-04 00:25:03 +0500

    Update Different174.cpp

--- Homework14/Different174.cpp
+++ Homework14/Different174.cpp
@@ -9,38 +9,14 @@ bool b[N];
 
 int main() {
     cin >> n;
-    for (int i = 0; i < n; i++) {
-        int temp;
-        cin >> temp;
-        a.push_back(temp);
-    }
-    sort(a.begin(), a.end());
-    for (int i = 0 ; i < n; i++) {
-        if (a[i] != a[i+1]) {
-            cnt++;
-        }
-    }
-    cout << cnt;
-    return 0;
-}
-----------------------------------------------------------
-
-#include <bits/stdc++.h>
-
-using namespace std;
-
-const long long N = 1000004;
-long long n, a[N], cnt;
-bool b[N];
-
-int main() {
-    cin >> n;
+    a.resize(n);
     for (int i = 0; i < n; i++) {
         cin >> a[i];
-        b[a[i]] = 1;
     }
-    for (int i = 0;  i < n; i++) {
-        if (b[i] == 1) {
+    sort(a.begin(), a.end());
+    cnt = 1;
+    for (int i = 0 ; i + 1 < n; i++) {
+        if (a[i] != a[i+1]) {
             cnt++;
         }
     }
