12b68570cfe387e16b0f96b68e01147b92cccc95
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2024-05-19 16:10:33 +0500

    Create Coins2_Template.cpp

---
+++ Botalka/Coins2_Template.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m, ans = -1;
+vector <int> a;
+void bt(int cnt) { 
+    if (cnt == ..) {
+        if (check()) {
+            ans = //this int;
+        }
+    }
+    //bt
+    
+}
+
+int main(){
+    cin >> n >> m;
+    for (int i = 0; i < 2*m; i++) {
+        int temp;
+        cin >> temp;
+        a.push_back(temp);
+        a.push_back(temp);
+        checkans += a[i];
+    }
+    if (checkans < ans) {
+        cout << -1;
+        return 0;
+    }
+    bt(0);
+    if (ans == -1) {
+        cout << 0;
+        return 0;
+    }
+    cout << ans;
+    return 0;
+}
+
+
+
+
