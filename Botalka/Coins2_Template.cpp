9a209917282649c17e04f52b99bc2cf85cb44825
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2024-05-25 20:34:30 +0500

    Update Coins2.cpp

--- Botalka/Coins2_Template.cpp
+++ Botalka/Coins2_Template.cpp
@@ -2,39 +2,52 @@
 
 using namespace std;
 
-int n, m, ans = -1;
+int n, m, ans = -1, checkans;
+bool light = 1, backlight = 0;
+
 vector <int> a;
-void bt(int cnt) { 
-    if (cnt == ..) {
-        if (check()) {
-            ans = //this int;
+void bt(int cnt, int sum, int amount) { 
+    if (sum >= n) {
+        if (sum == n) {
+            if (light = 1) {
+                ans = amount;
+                light = 0;
+            }
+            backlight = 1;
         }
+        return;
     }
-    //bt
+    if (sum + a[cnt] <= n) {
+        bt(cnt+1, sum += a[cnt], amount++);
+    } else {
+        bt(cnt+1, sum, amount);
+    }
     
 }
 
 int main(){
     cin >> n >> m;
-    for (int i = 0; i < 2*m; i++) {
+    for (int i = 0; i < m; i++) {
         int temp;
         cin >> temp;
         a.push_back(temp);
         a.push_back(temp);
-        checkans += a[i];
+        checkans += 2*temp;
     }
-    if (checkans < ans) {
+    reverse(a.begin(), a.end());
+    if (checkans < n) {
         cout << -1;
         return 0;
     }
-    bt(0);
-    if (ans == -1) {
+    bt(0, 0, 0);
+    if (backlight = 0) {
         cout << 0;
         return 0;
     }
     cout << ans;
     return 0;
 }
+// задебажить не получается
 
 
 
