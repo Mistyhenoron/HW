186148e3a1eb2ebcb732c60f0cfe86b2cc61e7d4
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2024-05-26 10:52:14 +0500

    Update Coins2.cpp

--- Botalka/Coins2.cpp
+++ Botalka/Coins2.cpp
@@ -4,23 +4,24 @@ using namespace std;
 
 int n, m, ans = -1, checkans;
 bool light = 1, backlight = 0;
-
 vector <int> a;
+
 void bt(int cnt, int sum, int amount) { 
-    if (sum >= n) {
+    if (cnt == m) {
         if (sum == n) {
-            if (light = 1) {
+            if (light == 1) { // нам нужен первый вариант тк он самый лучший
                 ans = amount;
                 light = 0;
             }
-            backlight = 1;
+            backlight = 1; // если вариантов вообще не нашлось, то в ответе 0
         }
         return;
     }
+    
+    bt(cnt+1, sum, amount);
+    
     if (sum + a[cnt] <= n) {
         bt(cnt+1, sum += a[cnt], amount++);
-    } else {
-        bt(cnt+1, sum, amount);
     }
     
 }
@@ -34,20 +35,22 @@ int main(){
         a.push_back(temp);
         checkans += 2*temp;
     }
+    sort(a.begin(), a.end());
     reverse(a.begin(), a.end());
     if (checkans < n) {
         cout << -1;
         return 0;
     }
     bt(0, 0, 0);
-    if (backlight = 0) {
+    
+    if (backlight == 0) {
         cout << 0;
         return 0;
     }
     cout << ans;
     return 0;
 }
-// задебажить не получается
+
 
 
 
