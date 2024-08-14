ed4b10f26167ed088a792958eaf7a6e037c5ffea
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2024-08-14 18:20:36 +0500

    Create Numb_of_inversions(by_msort).cpp

---
+++ Course/Numb_of_inversions(by_msort).cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using vi = vector <int>;
+
+int n, ans;
+vi a;
+vi b;
+
+vi merge(vi t1, vi t2, int half) {
+    vi res;
+    int i = 0, j = 0;
+    while (i < t1.size() && j < t2.size()) {
+        if (t1[i] <= t2[j]) {
+            res.push_back(t1[i]);
+            i++;
+        }
+        else {
+            res.push_back(t2[j]);
+            j++;
+            ans += (half - i);
+        }
+    }
+    while (i < t1.size()) {
+        res.push_back(t1[i]);
+        i++;
+    }
+    while (j < t2.size()) {
+        res.push_back(t2[j]);
+        j++;
+    }
+    return res;
+}
+
+vi msort(vi t) {
+    if (t.size() <= 1) {
+        return t;
+    }
+    int half = t.size()/2;
+    return merge(msort(vi(t.begin(), t.begin()+half)),
+                 msort(vi(t.begin()+half, t.end())), half);
+}
+
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int temp;
+        cin >> temp;
+        a.push_back(temp);
+    }
+    b = msort(a);
+    cout << ans;
+    return 0;
+}
