8a7c3a9dc3f30161d008b2047e7425f0a72ce9df
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2024-08-20 00:12:15 +0500

    Update Segment_tree_minmax.cpp

--- Else/Segment_tree_minmax.cpp
+++ Else/Segment_tree_minmax.cpp
@@ -89,3 +89,93 @@ int main() {
 
     return 0;
 }
+
+-------------------------------------------------------------------------------------------------------------------------
+    #include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vvvi = vector<vector<vector<int>>>;
+#define p_b push_back
+const int INF = INT_MAX;
+const double eps = 1e-12;
+const int mod = 1073741824;
+
+const ll N = 1e5;
+ll a[N], treemax[4*N], treemin[4*N];
+int k;
+
+ll binpow(ll a, ll b) {
+    if (b == 0) return 1;
+    ll temp = binpow(a, b / 2);
+    temp = temp * temp;
+    if (b % 2 != 0) temp *= a;
+    return temp;
+}
+
+void build(int v, int l, int r) {
+    if (l == r - 1) {
+        treemin[v] = treemax[v] = a[l];
+        return;
+    }
+    int m = (l + r) / 2;
+    build(2 * v + 1, l, m);
+    build(2 * v + 2, m, r);
+    treemin[v] = min(treemin[2 * v + 1], treemin[2 * v + 2]);
+    treemax[v] = max(treemax[2 * v + 1], treemax[2 * v + 2]);
+}
+
+pair<ll, ll> minmax(int v, int l, int r, int askl, int askr) {
+    if (l >= askr || r <= askl) {
+        return {LLONG_MAX, LLONG_MIN};
+    }
+    if (l >= askl && r <= askr) {
+        return {treemin[v], treemax[v]};
+    }
+    int m = (l + r) / 2;
+    auto left = minmax(2 * v + 1, l, m, askl, askr);
+    auto right = minmax(2 * v + 2, m, r, askl, askr);
+    return {min(left.first, right.first), max(left.second, right.second)};
+}
+
+void change(int v, int l, int r, int pos, int val) {
+    if (l == r - 1) {
+        treemin[v] = treemax[v] = val;
+        return;
+    }
+    int m = (l + r) / 2;
+    if (pos < m) {
+        change(2 * v + 1, l, m, pos, val);
+    } else {
+        change(2 * v + 2, m, r, pos, val);
+    }
+    treemin[v] = min(treemin[2 * v + 1], treemin[2 * v + 2]);
+    treemax[v] = max(treemax[2 * v + 1], treemax[2 * v + 2]);
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> k;
+    for (int i = 0; i < N; ++i) {
+        a[i] = (binpow(i + 1, 2) % 12345) + (binpow(i + 1, 3) & 23456);
+    }
+    build(0, 0, N);
+    while (k--) {
+        int xi, yi;
+        cin >> xi >> yi;
+        if (xi > 0) {
+            auto ans = minmax(0, 0, N, xi - 1, yi);
+            cout << ans.second - ans.first << endl;
+        } else {
+            xi = -xi - 1;
+            change(0, 0, N, xi, yi);
+        }
+    }
+    return 0;
+}
+
+// где ошибка
