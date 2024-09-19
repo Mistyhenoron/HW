c813780db73a316d04ed3d22e20a66c46b8e7c8d
Mistyhenoron <138688690+Mistyhenoron@users.noreply.github.com>
2024-09-19 15:33:40 +0500

    Update Roman_dp.cpp

--- Botalka/Roman_dp.cpp
+++ Botalka/Roman_dp.cpp
@@ -4,32 +4,37 @@ using namespace std;
 using ll = long long;
 using vi = vector<ll>;
 using vvi = vector<vector<ll>>;
-#define p_b push_back
 const int INF = INT_MAX;
 
+int n, k;
+
 int main() {
-    ll n, k;
     cin >> n >> k;
-    vi pages(n);
+    vi p(n+1);
     for (int i = 0; i < n; i++) {
-        cin >> pages[i];
+        cin >> p[i];
     }
-    vvi dp(n + 1, vi(k + 1));
-    vi prefixSum(n + 1);
+    vi prefsum(n+1);
     for (int i = 1; i <= n; i++) {
-        prefixSum[i] = prefixSum[i - 1] + pages[i - 1];
+        prefsum[i] = prefsum[i-1] + p[i-1];
     }
+    vvi dp(n+1, vi(k+1));
     for (int i = 1; i <= n; i++) {
-        dp[i][1] = prefixSum[i];
-    }
+        dp[i][1] = prefsum[i];
+    }   
     for (int i = 1; i <= n; i++) {
-        for (int K = 2; K <= k; K++) {
-            dp[i][K] = INT_MAX;
-            for (int j = 0; j < i; j++) {
-                dp[i][K] = min(dp[i][K], max(dp[j][K - 1], prefixSum[i] - prefixSum[j]));
+        for (int j = 2; j <= k; j++) {
+            dp[i][j] = INF;
+            for (int l = 1; l < i; l++) {
+                dp[i][j] = min(dp[i][j], max(dp[l][j-1], prefsum[i] - prefsum[l]));
             }
         }
     }
-    cout << dp[n][k];
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= k; j++) {
+            cout << dp[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
-} // на acmp ОК а на cf почему то нет
+}
